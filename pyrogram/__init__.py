5ac9812ea8f2048a8a6feaa54938cd95942d97da
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-05-27 19:48:25 +0700

    update pyrogram/__init__.py

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -16,27 +16,11 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "2.0.106"
-__license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
-__copyright__ = "Copyright (C) 2017-present Dan <https://github.com/delivrance>"
+from importlib import import_module
 
-from concurrent.futures.thread import ThreadPoolExecutor
+from . import types, functions, base, core
+from .all import objects
 
-
-class StopTransmission(Exception):
-    pass
-
-
-class StopPropagation(StopAsyncIteration):
-    pass
-
-
-class ContinuePropagation(StopAsyncIteration):
-    pass
-
-
-from . import raw, types, filters, handlers, emoji, enums
-from .client import Client
-from .sync import idle, compose
-
-crypto_executor = ThreadPoolExecutor(1, thread_name_prefix="CryptoWorker")
+for k, v in objects.items():
+    path, name = v.rsplit(".", 1)
+    objects[k] = getattr(import_module(path), name)
