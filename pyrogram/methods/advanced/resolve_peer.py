12a25c9ef40e258280340bb850e0ae884b53c4da
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-05-28 08:50:57 +0700

    Update resolve_peer.py

--- pyrogram/methods/advanced/resolve_peer.py
+++ pyrogram/methods/advanced/resolve_peer.py
@@ -30,8 +30,7 @@ log = logging.getLogger(__name__)
 
 class ResolvePeer:
     async def resolve_peer(
-        self: "pyrogram.Client",
-        peer_id: Union[int, str]
+        self: "pyrogram.Client", peer_id: Union[int, str]
     ) -> Union[raw.base.InputPeer, raw.base.InputUser, raw.base.InputChannel]:
         """Get the InputPeer of a known peer id.
         Useful whenever an InputPeer type is required.
@@ -47,7 +46,7 @@ class ResolvePeer:
         Parameters:
             peer_id (``int`` | ``str``):
                 The peer id you want to extract the InputPeer from.
-                Can be a direct id (int), a username (str) or a phone number (str).
+                Can be a direct id (int), a username (str) or a phone number (str) or *t.me/<username>* link.
 
         Returns:
             ``InputPeer``: On success, the resolved peer id is returned in form of an InputPeer object.
@@ -66,6 +65,7 @@ class ResolvePeer:
                     return raw.types.InputPeerSelf()
 
                 peer_id = re.sub(r"[@+\s]", "", peer_id.lower())
+                peer_id = re.sub(r"https://t.me/", "", peer_id.lower())
 
                 try:
                     int(peer_id)
@@ -74,9 +74,7 @@ class ResolvePeer:
                         return await self.storage.get_peer_by_username(peer_id)
                     except KeyError:
                         await self.invoke(
-                            raw.functions.contacts.ResolveUsername(
-                                username=peer_id
-                            )
+                            raw.functions.contacts.ResolveUsername(username=peer_id)
                         )
 
                         return await self.storage.get_peer_by_username(peer_id)
@@ -92,28 +90,18 @@ class ResolvePeer:
                 await self.fetch_peers(
                     await self.invoke(
                         raw.functions.users.GetUsers(
-                            id=[
-                                raw.types.InputUser(
-                                    user_id=peer_id,
-                                    access_hash=0
-                                )
-                            ]
+                            id=[raw.types.InputUser(user_id=peer_id, access_hash=0)]
                         )
                     )
                 )
             elif peer_type == "chat":
-                await self.invoke(
-                    raw.functions.messages.GetChats(
-                        id=[-peer_id]
-                    )
-                )
+                await self.invoke(raw.functions.messages.GetChats(id=[-peer_id]))
             else:
                 await self.invoke(
                     raw.functions.channels.GetChannels(
                         id=[
                             raw.types.InputChannel(
-                                channel_id=utils.get_channel_id(peer_id),
-                                access_hash=0
+                                channel_id=utils.get_channel_id(peer_id), access_hash=0
                             )
                         ]
                     )
@@ -123,3 +111,4 @@ class ResolvePeer:
                 return await self.storage.get_peer_by_id(peer_id)
             except KeyError:
                 raise PeerIdInvalid
+            
