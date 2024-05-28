9ac286122cd92584ff70fb2abed05a66f47e0eb6
jonesroot <jonesroot@users.noreply.github.com>
2024-05-28 00:56:04 +0000

    Auto Bot

--- pyrogram/types/user_and_chats/birthday.py
+++ pyrogram/types/user_and_chats/birthday.py
@@ -19,6 +19,7 @@
 from typing import Optional
 
 from pyrogram import raw
+
 from ..object import Object
 
 
@@ -36,27 +37,16 @@ class Birthday(Object):
             Birthday year.
     """
 
-    def __init__(
-        self,
-        *,
-        day: int,
-        month: int,
-        year: int = None
-
-    ):
+    def __init__(self, *, day: int, month: int, year: int = None):
         self.day = day
         self.month = month
         self.year = year
 
     @staticmethod
-    def _parse(
-        birthday: "raw.types.Birthday" = None
-    ) -> Optional["Birthday"]:
+    def _parse(birthday: "raw.types.Birthday" = None) -> Optional["Birthday"]:
         if not birthday:
             return
 
         return Birthday(
-            day=birthday.day,
-            month=birthday.month,
-            year=getattr(birthday, "year", None)
+            day=birthday.day, month=birthday.month, year=getattr(birthday, "year", None)
         )
